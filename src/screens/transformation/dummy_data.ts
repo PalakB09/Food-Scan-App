6d1952669647497203c4ce9da50ae350f5b73922
PalakBakshi <palak.bakshi09@gmail.com>
2025-11-24 21:21:01 +0530

    update transformation screen

--- src/screens/transformation/dummy_data.ts
+++ src/screens/transformation/dummy_data.ts
@@ -6,33 +6,33 @@ type TransformationData = {
 
 export const DUMMY_TRANSFORMATION_DATA: TransformationData[] = [
   {
-    date: "2023-12-31",
-    url: `https://i2-prod.mirror.co.uk/incoming/article8144525.ece/ALTERNATES/n310p/PAY-An-obese-mum-has-transformed-her-life-by-taking-selfie.jpg`,
+    date: "2024-12-31",
+    url: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
     weight: 100,
   },
   {
-    date: "2024-03-15",
-    url: `https://i2-prod.mirror.co.uk/incoming/article8144530.ece/ALTERNATES/n310p/PAY-An-obese-mum-has-transformed-her-life-by-taking-selfie.jpg`,
+    date: "2025-03-15",
+    url: "https://images.unsplash.com/photo-1558611848-73f7eb4001a1",
     weight: 93,
   },
   {
-    date: "2024-06-14",
-    url: `https://i2-prod.mirror.co.uk/incoming/article8144524.ece/ALTERNATES/n310p/PAY-An-obese-mum-has-transformed-her-life-by-taking-selfie.jpg`,
+    date: "2025-06-14",
+    url: "https://images.unsplash.com/photo-1554284126-aa88f22d8b74",
     weight: 82,
   },
   {
-    date: "2024-08-11",
-    url: `https://i2-prod.mirror.co.uk/incoming/article8144527.ece/ALTERNATES/n310p/PAY-An-obese-mum-has-transformed-her-life-by-taking-selfie.jpg`,
+    date: "2025-08-11",
+    url: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
     weight: 75,
   },
   {
-    date: "2024-09-12",
-    url: `https://i2-prod.mirror.co.uk/incoming/article8144523.ece/ALTERNATES/n310p/PAY-An-obese-mum-has-transformed-her-life-by-taking-selfie.jpg`,
+    date: "2025-09-12",
+    url: "https://images.unsplash.com/photo-1558611848-73f7eb4001a1",
     weight: 68,
   },
   {
-    date: "2024-12-11",
-    url: `https://i2-prod.mirror.co.uk/incoming/article8144529.ece/ALTERNATES/n310p/PAY-An-obese-mum-has-transformed-her-life-by-taking-selfie.jpg`,
+    date: "2025-12-11",
+    url: "https://images.unsplash.com/photo-1554284126-aa88f22d8b74",
     weight: 60,
   },
 ] as const;
